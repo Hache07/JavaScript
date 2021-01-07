4265b147c941864b15eb9429dd20ff10de2de640
Hector <atche1603@gmail.com>
2021-01-07 10:11:32 -0400

    Estructuras de control (operadores relacionales)

--- Estructuras de Control/js/main.js
+++ Estructuras de Control/js/main.js
@@ -5,5 +5,27 @@
 let a = 5+(5-10)*3;
 let modulo = 5 % 2;
 
-console.log(a);
-console.log(`El modulo de la division 5/2 es: ${modulo}`);
+// console.log(a);
+// console.log(`El modulo de la division 5/2 es: ${modulo}`);
+
+/*
+    Operadores relacionales < > <= >= = == === != !==
+    = Un igual es asiganación de variables.
+    == Dos iguales es comparación de valores.
+    === Tres iguales (Comparación estricta) es comparación de valores y tipos de datos.
+*/
+
+console.log(8 > 9);
+console.log(9 > 8);
+console.log(8 >= 9);
+console.log(9 >= 8);
+console.log(7 > 7);
+console.log(7 >= 7);
+
+console.log(7 == 7);
+console.log("7" == 7);
+console.log(0 == false);
+
+console.log(7 === 7);
+console.log("7" === 7);
+console.log(0 === false);
