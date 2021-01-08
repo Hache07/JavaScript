19c15162529f207b571dd8a0c3b2da892e3996c5
Hector <atche1603@gmail.com>
2021-01-08 09:18:25 -0400

    Estructuras de control (operador ternario)

--- Estructuras de Control/js/condicionales.js
+++ Estructuras de Control/js/condicionales.js
@@ -17,26 +17,35 @@
 
 /* if - else if - else (if anidados)*/
 
-let hora = 15;
+// let hora = 19;
 
-if(hora > 0 && hora < 6) {
-    console.log(`Son las ${hora} déjame dormir por favor.`);
-} else if(hora > 5 && hora < 12) {
-    console.log('Buenos días.');
-} else if(hora >= 12 && hora < 19) {
-    console.log('Buenas tardes.');
-} else {
-    console.log('Buenas noches.');
-}
+// if(hora > 0 && hora < 6) {
+//     console.log(`Son las ${hora} déjame dormir por favor.`);
+// } else if(hora > 5 && hora < 12) {
+//     console.log('Buenos días.');
+// } else if(hora >= 12 && hora < 19) {
+//     console.log('Buenas tardes.');
+// } else {
+//     console.log('Buenas noches.');
+// }
+
+// console.log('------ OPERADOR TERNARIO ------');
+
+// let result = (hora > 0 && hora < 6)
+//     ? `Son las ${hora} déjame dormir por favor.`
+//     : (hora > 5 && hora < 12)
+//         ? 'Buenos días.'
+//         : (hora >= 12 && hora < 19)
+//             ? 'Buenas tardes.'
+//             : 'Buenas noches.';
+
+// console.log(result);
+
+/* Operador ternario (condicion) ? sentencia_1 : sentencia_2 */
 
 console.log('------ OPERADOR TERNARIO ------');
 
-let result = (hora > 0 && hora < 6)
-    ? console.log(`Son las ${hora} déjame dormir por favor.`)
-    : (hora > 5 && hora < 12)
-        ? console.log('Buenos días.')
-        : (hora >= 12 && hora < 19)
-            ? console.log('Buenas tardes.')
-            : console.log('Buenas noches.');
+let edad = 25;
+let eresMayor = (edad >= 18) ? 'Eres mayor de edad.' : 'Eres menor de edad.';
 
-/* Operador ternario */
+console.log(eresMayor);
