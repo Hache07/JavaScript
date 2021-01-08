a665c90f1a35849d720fac2ce072a57f85e03e32
Hector <atche1603@gmail.com>
2021-01-08 10:26:56 -0400

    Estrucutras de control (for)

--- Estructuras de Control/js/ciclos.js
+++ Estructuras de Control/js/ciclos.js
@@ -1,4 +1,5 @@
-/* while y do - while 
+/* 
+    while y do - while 
     while: se ejecuta siempre y cuando la condición se cumpla.
     do - while: se ejecuta almenos una ves las instrucciones que estan dentro del do.
 */
@@ -6,12 +7,23 @@
 let contador = 0;
 
 while(contador < 10) {
-    console.log("while "+contador);
+    //console.log("while "+contador);
     contador++;
 }
 
 do {
-    console.log("do while "+contador);
+    //console.log("do while "+contador);
     contador++;
 } while (contador < 10);
 
+/* for */
+
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let frutas = ["uva", "pera", "manzana", "fresa", "pomelo", "sandia"];
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
